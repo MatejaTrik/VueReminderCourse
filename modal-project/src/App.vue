75552aecec3df8a647bde9a7356648b86879a221
MatejaTrik <mtrikic2@gmail.com>
2021-03-23 18:06:32 +0100

    23.03.2021.

---
+++ modal-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <h1 class="text-center">{{title}}</h1>
+
+  <Modal />
+
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'my first vue app :))'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('bg-warning')
+      this.$refs.name.focus()
+    }
+  },
+}
+</script>
+
+<style>
+
+
+
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal content </p>
+            <h1>ALOO</h1>
+
+
+        </div>
+
+
+        <div class="model">
+            <p>Modal Content</p>
+            <h1>Aleeeeo</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .model {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+
+    h1{
+        color: #03cfb4;
+        padding: none;
+        padding: 0;
+    }
+</style>
