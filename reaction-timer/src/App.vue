70c318b9da278a14c531de824609d678ad2c1746
MatejaTrik <mtrikic2@gmail.com>
2021-03-24 17:01:58 +0100

    24.03.2021.

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container-fluid text-center">
+    <h1 class="bg-primary text-dark p-3">Ninja Reaction Timer</h1>
+    <button class="btn btn-outline-primary w-50 mt-4" @click="start" :disabled="isPlaying">Start</button>
+    <block v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <h3 class="text-left bg-primary my-3 p-3 text-white">Vas Konacan Rezultat Je:{{ score }}</h3>
+  </div>
+</template>
+
+<script>
+import block from './components/block'
+
+export default {
+  name: 'App',
+  components: { block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true 
+      console.log(this.delay)
+    },
+    endGame(e) {
+      this.score = e,
+      this.isPlaying = false
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/block.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        <p class="bg-white text-primary" >Click Me</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            ractionTime: 0
+        }
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.ractionTime += 10
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.ractionTime)
+            this.$emit('end', this.ractionTime)
+        }
+    },
+    mounted() {
+        console.log(" COMPONTENTS MOUTNED ")
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+            console.log(this.delay)
+        }, this.delay)
+    },
+    updated() {
+        console.log(" COMPONTENTS UPDATED ")
+        
+    },
+    unmounted() {
+        console.log(" COMPONENTS UNMOUNTED  ")
+    },
+}
+</script>
+
+<style>
+
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #0d6efd;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+
+</style>
